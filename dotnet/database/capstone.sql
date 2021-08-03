e42410c8455969b36d79ba2f961b0e4f87a5410e
Charles Inkrott <charlesinkrott@gmail.com>
2021-08-03 09:26:36 -0400

    event table database

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -25,10 +25,20 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 )
 
+CREATE TABLE events (
+	event_id int IDENTITY(1,1) NOT NULL,
+	name varchar(255) NOT NULL,
+	description varchar(255) NOT NULL,
+	type varchar(50) NOT NULL,
+	period_in_days int NOT NULL
+	)
+
+
 --populate default data
 -- user/password
 INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user', 'theofficialbobojones@gmail.com');
 --admin/password
 INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin', 'theofficialbobojones@gmail.com');
 
+INSERT INTO events (name, description, type, period_in_days) VALUES ('pellytonya', 'big ol bike ride', 'cycling', 7);
 GO

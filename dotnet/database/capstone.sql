6256c28170279f176482a65acb23204e36ce59cd
Mitchell Rhodes <mrhodes208@gmail.com>
2021-08-10 12:52:53 -0400

    Database Inserts

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -71,12 +71,90 @@ CREATE TABLE user_log(
 INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user', 'theofficialbobojones@gmail.com');
 --admin/password
 INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Billy','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Mike','gYax/nldlps+n/IL3paG1tnX0Jk= ', 'rDaoxV0X3rg=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('John','aFfNx8CzaVHSfFjSfM4Zx/ehF0c= ', 'lFYlQhiR3/Q=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Sarah','tF8wj5J9u9pEt7qg9sMi46i5Vwg= ', 'bB2hzm+kWxw=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Mia','O0WnyUuipLQayYpF5G4ncL/oR4Q=', 'F4y+FwW1P+Q=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Luke','I8YxsZAwEcz4Oiy/4v6H5nUZ6C8=', 'VG1l+Xiboi8=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Kurt','kv6yYuRQmHKJ5aFq5B6qNwZG8+g=', 'FUEq3Hb//qw=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Teresa','3uzzKuAjxA0A/a4a15RtOLxGvyM=', 'kaTEMu7zNjw=','admin', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Tom','sZUFmSEGWD7TRygtc68rreWiP9o', 'Ys9CrV56YSg=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Andrew','pipfCNqnyaPHTPKxoVQ1+Yn7qks=', 'oeY7C+qU448=','user', 'theofficialbobojones@gmail.com');
+INSERT INTO users (username, password_hash, salt, user_role, email) VALUES ('Audra','qIkLG7VXclqW1COmZdaFR7WVpuY=', 'UaIVI1N6CfQ=','user', 'theofficialbobojones@gmail.com');
 
+
+--Events
 INSERT INTO events (name, description, type, period_in_days) VALUES ('pellytonya', 'big ol bike ride', 'cycling', 7);
 
-INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 1);
+INSERT INTO events (name, description, type, period_in_days) VALUES ('Mega-Marathon', 'Walk 500 Miles', 'Walking', 15);
 
-INSERT INTO user_events(event_id,user_id) VALUES (1,1)
+INSERT INTO events (name, description, type, period_in_days) VALUES ('400m Medley', 'All four swimming strokes', 'Swimming', 4);
+
+INSERT INTO events (name, description, type, period_in_days) VALUES ('Charity Walk', 'Walk for your favorite local cause', 'Walking', 1);
+
+INSERT INTO events (name, description, type, period_in_days) VALUES ('High School Fun Run', 'Revisit your old stomping grounds and run with friends', 'running', 1);
+
+INSERT INTO events (name, description, type, period_in_days) VALUES ('Swim Lake Erie', 'Take your life into your hands and swim in Lake Erie', 'Swimming', 2);
+
+INSERT INTO events (name, description, type, period_in_days) VALUES ('Cycle Across the USA', 'Can you cycle from California to Maine?', 'cycling', 15);
+
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 1);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 1);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 1);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 1);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 2);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 2);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 3);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 3);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 4);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 4);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 5);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 5);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 6);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 6);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 7);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 7);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 8);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 8);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 9);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 9);
+
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('Marathon', 'Run a marathon', 'speedwalking', 5, 10);
+INSERT INTO goals (goal_name, goal_description, goal_type, period_in_days, user_id) VALUES ('10K', 'Run 6.2 Miles', 'Fun Run', 5, 10);
+
+
+
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (1,1, 5)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (3,1, 5)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (2,1, 4)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (4,1, 3)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (5,3, 6)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (2,3, 7)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (3,3, 3)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (4,9, 4)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (1,8, 7)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (2,9, 5)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (2,8, 19)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (5,11, 0)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (4,11, 3)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (2,11, 2)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (5,12, 8)
+INSERT INTO user_events(event_id,user_id, distance_progress) VALUES (4,12, 9)
+
+
+
+
 
 ALTER TABLE goals ADD FOREIGN KEY(user_id)
 REFERENCES users(user_id)

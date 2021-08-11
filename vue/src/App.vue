2973d5c7da92dbef3a040678050e5ff3ff2e3adc
Charles Inkrott <charlesinkrott@gmail.com>
2021-08-11 12:56:23 -0400

    navbar logo

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" v-bind:style="{ backgroundColor: a4bdc5}">
     <nav id="navbar-design" class="navbar navbar-expand-lg fixed-top">
       <div class="container-fluid">
+        <img class="navbar-brand" src="https://i.ibb.co/mqm8QS9/summit-Logo-Cropped.png" alt="..." height="55">
         <button
           class="navbar-toggler"
           type="button"
@@ -139,7 +140,7 @@ export default {
 }
 
 #navbar-design {
-  background-color: #12292F;
+  background-color: #2D474D;
   color: #ffffff;
 }
 nav .navbar-nav li a {

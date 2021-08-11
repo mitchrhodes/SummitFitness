32f8db54e76c714ff90ee87001d142ac7ea992b9
Mitchell Rhodes <mrhodes208@gmail.com>
2021-08-11 11:10:19 -0400

    Databse Changes

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -7,7 +7,7 @@
     <br />
     <h3 class="text-center">Welcome to Summit Fitness</h3>
     <br />
-    <h6 class="text-center mx-5">We all have our mountains to climb.  But no matter the size of the obstacle you're looking to overcome, the only way to get there is one step at a time. 
+    <h6 class="text-center mx-5" >We all have our mountains to climb.  But no matter the size of the obstacle you're looking to overcome, the only way to get there is one step at a time. 
        At Summit Fitness, we're here for you every step you take on your fitness journey.  Whether you are looking to see how you stack up against the competition in our virtual events or
        have a distance goal you are trying to reach for the first time ever, let us help you reach your peak.  Track your progress, see results. 
 </h6>
